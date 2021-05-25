43700a588ba85fba6806448dd2b15fb357ee92ef
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-25 15:16:24 -0300

    Organizando a estrutura das variaveis

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,23 +4,23 @@ import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-describe('1 - Teste o Componente <App.js /> ', () => {
+describe('1 - Test the Component <App.js /> ', () => {
   test('shows the Pokédex when the route is `/`', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText('Home'));
     const routeHome = history.location.pathname;
+    fireEvent.click(getByText('Home'));
     expect(routeHome).toBe('/');
   });
   test('shows the Pokédex when the route is `/about`', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText('About'));
     const routeHome = history.location.pathname;
+    fireEvent.click(getByText('About'));
     expect(routeHome).toBe('/about');
   });
   test('shows the Pokédex when the route is `/favorites`', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    fireEvent.click(getByText('Favorite Pokémons'));
     const routeHome = history.location.pathname;
+    fireEvent.click(getByText('Favorite Pokémons'));
     expect(routeHome).toBe('/favorites');
   });
 });
