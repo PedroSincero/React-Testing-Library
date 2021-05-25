d7f1d64de69e956358c8a89a5951f94e05e7b492
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-25 16:45:31 -0300

    Adicionando Teste 2.3 & Finalizando testes

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -23,8 +23,12 @@ describe('2 - Test whether the page contains information about Pokédex ', () =>
     expect(getParagraphs1).toBeInTheDocument();
     expect(getParagraphs2).toBeInTheDocument();
   });
-  // test('2.3 - Test if the page contains the following image of a Pokédex.', () => {
-  //   const { getByText } = renderWithRouter(<About />);
-
-  // });
+  test('2.3 - Test if the page contains the following image of a Pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const img = getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(img).toHaveAttribute('alt', 'Pokédex');
+  });
 });
+
+// Site que me Ajudou https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
