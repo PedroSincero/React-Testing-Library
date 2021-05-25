0f278d52851e482ea2829407cad9fa0cfc3b28d6
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-25 16:39:17 -0300

    Adicionando Teste 2.2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { fireEvent } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('2 - Test whether the page contains information about Pokédex ', () => {
+  test('2.1 - Test if the page contains an h2 with the text About Pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const getH2 = getByRole('heading', { level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(getH2).toBeInTheDocument();
+  });
+  test('2.2 - Test if the page contains two paragraphs with text about Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const paragraphs1 = 'This application simulates a Pokédex, a';
+    const paragraphs2 = ' digital encyclopedia containing all Pokémons';
+    const textP1 = 'One can filter Pokémons by type,';
+    const textP2 = ' and see more details for each one of them';
+    const getParagraphs1 = getByText(paragraphs1 + paragraphs2);
+    const getParagraphs2 = getByText(textP1 + textP2);
+    expect(getParagraphs1).toBeInTheDocument();
+    expect(getParagraphs2).toBeInTheDocument();
+  });
+  // test('2.3 - Test if the page contains the following image of a Pokédex.', () => {
+  //   const { getByText } = renderWithRouter(<About />);
+
+  // });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,22 +2,22 @@ import React from 'react';
 // import { MemoryRouter } from 'react-router-dom';
 import { fireEvent } from '@testing-library/react';
 import App from '../App';
-import renderWithRouter from '../renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 
 describe('1 - Test the Component <App.js /> ', () => {
-  test('shows the Pokédex when the route is `/`', () => {
+  test('1.1 - shows the Pokédex when the route is `/`', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const routeHome = history.location.pathname;
     fireEvent.click(getByText('Home'));
     expect(routeHome).toBe('/');
   });
-  test('shows the Pokédex when the route is `/about`', () => {
+  test('1.2 - shows the Pokédex when the route is `/about`', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const routeHome = history.location.pathname;
     fireEvent.click(getByText('About'));
     expect(routeHome).toBe('/about');
   });
-  test('shows the Pokédex when the route is `/favorites`', () => {
+  test('1.3 - shows the Pokédex when the route is `/favorites`', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const routeHome = history.location.pathname;
     fireEvent.click(getByText('Favorite Pokémons'));
