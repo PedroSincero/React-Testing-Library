443ff338fca2bbc65d30fb05154fcca4ac5a5da7
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-25 17:11:11 -0300

    Ajustando Ordem de execuçao

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,26 +1,27 @@
 import React from 'react';
 // import { MemoryRouter } from 'react-router-dom';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('1 - Test the Component <App.js /> ', () => {
   test('1.1 - shows the Pokédex when the route is `/`', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const routeHome = history.location.pathname;
     fireEvent.click(getByText('Home'));
+    const routeHome = history.location.pathname;
     expect(routeHome).toBe('/');
   });
   test('1.2 - shows the Pokédex when the route is `/about`', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    const routeHome = history.location.pathname;
-    fireEvent.click(getByText('About'));
-    expect(routeHome).toBe('/about');
+    const { getByText, history, getByRole } = renderWithRouter(<App />);
+    const linkAbout = getByText('About');
+    fireEvent.click(linkAbout);
+    const routeAbout = history.location.pathname;
+    expect(routeAbout).toBe('/about');
   });
   test('1.3 - shows the Pokédex when the route is `/favorites`', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const routeHome = history.location.pathname;
     fireEvent.click(getByText('Favorite Pokémons'));
-    expect(routeHome).toBe('/favorites');
+    const routeFavorite = history.location.pathname;
+    expect(routeFavorite).toBe('/favorites');
   });
 });
