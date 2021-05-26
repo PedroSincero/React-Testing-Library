6c54ef6cb76c44b43be50c7d200128eb93412486
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-26 13:21:05 -0300

    Adicionando Alteraçoes no Desafio 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 // import { MemoryRouter } from 'react-router-dom';
-// import { fireEvent } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from './renderWithRouter';
-// import App from '../App';
+import App from '../App';
 
 describe('3 - Test the <Favorite Pokemons.js /> component ', () => {
   test('3.1 - message No favorite pokemon found is displayed on the screen', () => {
@@ -11,18 +11,14 @@ describe('3 - Test the <Favorite Pokemons.js /> component ', () => {
     const pokemonFavoriteNull = getByText('No favorite pokemon found');
     expect(pokemonFavoriteNull).toHaveTextContent('No favorite pokemon found');
   });
-  // test('3.2 - Test whether all your favorite Pokémon cards are displayed', () => {
-  //   const { getAllByRole, getByRole, getAllByTestId } = renderWithRouter(<App />);
-  //   const testId = getAllByTestId('pokemon-name');
-  //   const button = getAllByRole('button');
-
-  //   fireEvent.click(button[1]);
-  //   expect(button[1]).toHaveTextContent(/Electric/i);
-
-  //   // fireEvent.click(getByText(/More details/i));
-  //   // const check = getByRole('checkbox');
-
-  //   fireEvent.click(check);
-  //   // fireEvent.click(getByText(/Favorite Pokémons/i));
-  // });
+  test('3.2 - Test whether all your favorite Pokémon cards are displayed', () => {
+    const { getByText, getByRole, getAllByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/More details/i));
+    fireEvent.click(getByRole('checkbox'));
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const idName = getAllByTestId('pokemon-name');
+    expect(idName.length).toBe(1);
+  });
 });
+
+// Agradecimentos ao Raphael Guimeri Turma 10 - Tribo B
